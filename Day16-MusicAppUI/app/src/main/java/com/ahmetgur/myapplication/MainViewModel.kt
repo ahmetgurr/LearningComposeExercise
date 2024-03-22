1e1dd3c110ae8ff589a83cd927e5533961e51cc1
ahmetgurr <ahhmetgur@gmail.com>
2024-03-22 13:39:36 +0300

    Day16-MusicAppUI
    Day16-MusicAppUI

---
+++ Day16-MusicAppUI/app/src/main/java/com/ahmetgur/myapplication/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.ahmetgur.myapplication
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    private val _currentScreen: MutableState<Screen> = mutableStateOf(Screen.DrawerScreen.Account)
+
+
+    val currentScreen: MutableState<Screen>
+        get() = _currentScreen
+
+    fun setCurrentScreen(screen: Screen){
+        _currentScreen.value = screen
+    }
+
+
+}
